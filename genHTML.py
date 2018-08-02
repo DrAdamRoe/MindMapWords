adacd773265747a114e14a58c4e0d33c852cc407
Adam Roe <adam.roe@code.berlin>
2018-08-02 15:36:43 +0200

    first commit

---
+++ genHTML.py
@@ -0,0 +1,36 @@
+def genHTML(words_list):
+
+
+    out_string="""
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Wordslist</title>
+            <style>
+               body {font-family: Verdana, Geneva, sans-serif}
+               p {margin-left: 20px}
+            </style>
+        </head>
+        <body>
+        <h3> Words for a Software Engineering Mind Map </h3>
+    """
+
+    for word in range(len(words_list)):
+       out_string+="""
+       <p>%s</p>
+       """ % words_list[word]
+
+    out_string+="""
+        
+    </body>
+    </html>
+
+    """
+
+    output_file = "test/out.html"
+    fout = open(output_file, "w")
+    fout.write(out_string)
+    fout.close()
+
+    print("File %s written" % output_file)
---
+++ main.py
@@ -0,0 +1,27 @@
+#### A neat way to generate a ton of A5 files from a list of words, one entry per file.
+#### Built for a mindmap at the CODE Summer School
+
+
+# keep it clean, several self-defined functions with lots of HTMl and EPS formatting
+# import style adapted from solution 7 https://stackoverflow.com/questions/2349991/how-to-import-other-python-files
+
+from genHTML import genHTML
+
+
+# read in external text file line by line.
+# adapted from https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list
+
+def getWords(filename):
+    with open(filename) as file:
+        content = file.read().splitlines()
+
+    print("File %s opened and read" % filename)
+
+    return content
+
+def main():
+    words_list = getWords("input/words.txt")
+    genHTML(words_list)
+
+if __name__== "__main__":
+    main()
