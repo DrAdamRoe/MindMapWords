370089b09e5e773d478c616c807d2c4f70854b28
Adam Roe <adam.roe@code.berlin>
2018-08-03 11:57:48 +0200

    move getWords function to own file, improve comments

--- main.py
+++ main.py
@@ -5,43 +5,33 @@
 # keep it clean, several self-defined functions with lots of HTMl and EPS formatting
 # import style adapted from solution 7 https://stackoverflow.com/questions/2349991/how-to-import-other-python-files
 
+import sys
 from genHTML import genHTML
 from genSinglePDF import genSinglePDF
-
-
-# function to read external text file, clean, and return as list
-def getWords(filename):
-
-    # read in external text file line by line.
-    # adapted from https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list
-
-    with open(filename) as file:
-        content = file.read().splitlines()
-
-    # remove duplicates from words list
-    #interesting regarding collections on https://stackoverflow.com/questions/7961363/removing-duplicates-in-lists
-    #going with old fashined set approach because versioning hell
-    content_set = list(set(content))
-
-    # remove any trailing blank lines
-    content_set.remove("")
-
-    content_set.sort()
-
-    print("File %s opened and read. Final list contains %i words." % (filename, len(content_set)))
-
-    return content_set
+from getWords import getWords
 
 def main():
 
     inp = "input/words.txt"
+
     print("""
     Generating a set of printable files with 1 word per page from %s         
     """ % inp)
+
+    # read in text file
     words_list = getWords(inp)
+
+    # generate html (for sharing)
     genHTML(words_list)
+
+    # generate PDFs (for printing)
     for word in words_list:
         genSinglePDF(word)
+
+    print("""
+    Succesfully generated files to print. bye now.
+    """)
+    sys.exit(0)
 
 if __name__== "__main__":
     main()
