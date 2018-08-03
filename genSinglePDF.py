5b2c753667efeecfb1abc12a080190482a4d354c
Adam Roe <adam.roe@code.berlin>
2018-08-03 11:55:56 +0200

    rotate, fix sizing

--- genSinglePDF.py
+++ genSinglePDF.py
@@ -10,8 +10,11 @@ def genSinglePDF(word):
     print("Processsing: %s" % word )
     out_string="""
 %%!
-/Helvetica findfont 50 scalefont setfont
-100 200 moveto
+/Helvetica findfont 
+50 scalefont 
+setfont
+90 rotate
+100 -222 moveto
 (%s) show
 
 showpage
@@ -24,14 +27,17 @@ showpage
     fout.write(out_string)
     fout.close()
 
-    command = "ps2pdf print/temp.eps print/%s.pdf" % word.replace(" ", "") #replace blanks with nothing
+    command = "ps2pdf -sPAPERSIZE=a5 print/temp.eps print/%s.pdf" % word.replace(" ", "") #replace blanks with nothing
 
     return_val = subprocess.call(command, shell=True)
 
+    #throw an error and retain eps
     if (return_val):
         print("""Error: "%s" returned %s. """ % (command,return_val))
         print("Exiting.")
         sys.exit(1)
+
+
 
    # print(out_string)
 
