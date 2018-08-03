08a0cd80d938936e230c9701ff40590ad674d7a7
Adam Roe <adam.roe@code.berlin>
2018-08-03 11:31:06 +0200

    remove blanks and duplicates from list, sort, and remove a pesky slash from the words list

--- main.py
+++ main.py
@@ -9,19 +9,36 @@ from genHTML import genHTML
 from genSinglePDF import genSinglePDF
 
 
-# read in external text file line by line.
-# adapted from https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list
-
+# function to read external text file, clean, and return as list
 def getWords(filename):
+
+    # read in external text file line by line.
+    # adapted from https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list
+
     with open(filename) as file:
         content = file.read().splitlines()
 
-    print("File %s opened and read" % filename)
+    # remove duplicates from words list
+    #interesting regarding collections on https://stackoverflow.com/questions/7961363/removing-duplicates-in-lists
+    #going with old fashined set approach because versioning hell
+    content_set = list(set(content))
 
-    return content
+    # remove any trailing blank lines
+    content_set.remove("")
+
+    content_set.sort()
+
+    print("File %s opened and read. Final list contains %i words." % (filename, len(content_set)))
+
+    return content_set
 
 def main():
-    words_list = getWords("input/words.txt")
+
+    inp = "input/words.txt"
+    print("""
+    Generating a set of printable files with 1 word per page from %s         
+    """ % inp)
+    words_list = getWords(inp)
     genHTML(words_list)
     for word in words_list:
         genSinglePDF(word)
